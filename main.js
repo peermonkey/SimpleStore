28b35fccdd5516d6edb46bc08ffed7081d816a9e
Stan <jangaleabhiwork@gmail.com>
2023-08-05 16:53:33 +0530

    cart function working

--- main.js
+++ main.js
@@ -27,13 +27,15 @@ let shopItemsData = [{
     price: 45000,
     desc: "I love you 78900",
     img: "images/img-4.jpg"
-}]
+}];
+
+let basket = [];
 
 let generateShop = ()=>{
     return (shop.innerHTML= shopItemsData.map((x)=>{
        let{id,name,price,desc,img}=x;
         return`
-        <div class="item">
+        <div id=product-id${id} class="item">
             <img width="220" src=${img} alt="">
             <div class="details">
                 <h3>${name}</h3>
@@ -41,9 +43,9 @@ let generateShop = ()=>{
                 <div class="price-quantity">
                     <h2>$${price}</h2>
                     <div class="buttons">
-                        <i class="bi bi-dash-lg"></i>
-                        <div class="quantity">0</div>
-                        <i class="bi bi-plus-lg"></i>
+                        <i onclick ="decrement(${id})" class="bi bi-dash-lg"></i>
+                        <div id=${id} class="quantity">0</div>
+                        <i onclick ="increment(${id})" class="bi bi-plus-lg"></i>
                     </div>
                 </div>
             </div>
@@ -53,3 +55,44 @@ let generateShop = ()=>{
 }
 
 generateShop();
+
+let increment = (id) =>{
+    let selectedItem = id;
+    let search =basket.find((x)=> x.id === selectedItem.id);
+    if(search === undefined){
+
+    
+    basket.push({
+        id: selectedItem.id,
+        item :1,
+    })}
+    else{
+        search.item +=1;
+
+    }
+    
+//console.log(basket)
+update(selectedItem.id);
+};
+let decrement = (id) =>{
+    let selectedItem = id;
+    let search =basket.find((x)=> x.id === selectedItem.id);
+    if(search.item === 0){return}
+    else{
+        search.item -=1;
+    }
+    
+//console.log(basket)
+update(selectedItem.id);
+};
+let update = (id) =>{
+    let search = basket.find((x)=>x.id === id);
+    document.getElementById(id).innerHTML =search.item;
+    calculation();
+};
+
+let calculation =()=>{
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0)
+    
+}
