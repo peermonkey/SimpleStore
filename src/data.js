3738eaf3940271de4b483e91b4f7808589c32086
Stan <jangaleabhiwork@gmail.com>
2023-08-09 16:51:34 +0530

    project complete

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "item1",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "It is a nice casual shirt",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "item2",
+      name: "Office Shirt",
+      price: 100,
+      desc: "It is a nice office shirt",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "item3",
+      name: "T Shirt",
+      price: 25,
+      desc: "It is a nice T shirt",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "item4",
+      name: "Mens Suit",
+      price: 300,
+      desc: "It is a nice men's suit",
+      img: "images/img-4.jpg",
+    },
+  ];
